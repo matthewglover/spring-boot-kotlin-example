85a72e380d1c5afbf2df65ea17ddc3f44881fab0
Matt Glover <mglover@8thlight.com>
2019-12-30 14:21:55 +0000

    Add simple JUnit test

---
+++ app/src/test/kotlin/com/matthewglover/app/SimpleTest.kt
@@ -0,0 +1,11 @@
+package com.matthewglover.app
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class SimpleTest {
+    @Test
+    internal fun foo() {
+        assertTrue(true)
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 repositories {
@@ -21,6 +23,57 @@ allprojects {
     }
 }
 
+// Dependencies
+subprojects {
+    apply(plugin = "kotlin")
+
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        "implementation"(kotlin("stdlib-jdk8"))
+        "implementation"(platform("org.springframework.boot:spring-boot-dependencies:2.2.2.RELEASE"))
+        "testImplementation"("org.junit.jupiter:junit-jupiter")
+    }
+}
+
+// Test configuration
+subprojects {
+    tasks.withType<Test> {
+        useJUnitPlatform()
+
+        testLogging {
+            events(
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STANDARD_OUT
+            )
+            exceptionFormat = TestExceptionFormat.FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+            showStandardStreams = true
+
+            debug {
+                events(
+                        TestLogEvent.STARTED,
+                        TestLogEvent.FAILED,
+                        TestLogEvent.PASSED,
+                        TestLogEvent.SKIPPED,
+                        TestLogEvent.STANDARD_OUT,
+                        TestLogEvent.STANDARD_ERROR
+                )
+                exceptionFormat = TestExceptionFormat.FULL
+            }
+
+            info.events = debug.events
+            info.exceptionFormat = debug.exceptionFormat
+        }
+    }
+}
+
 // Kotlin compilation configuration
 subprojects {
     tasks.withType<KotlinCompile> {
@@ -33,7 +86,6 @@ subprojects {
 
 // App config
 project(":app") {
-    apply(plugin = "kotlin")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 
     repositories {
@@ -41,8 +93,6 @@ project(":app") {
     }
 
     dependencies {
-        "implementation"(kotlin("stdlib-jdk8"))
-        "implementation"(platform("org.springframework.boot:spring-boot-dependencies:2.2.2.RELEASE"))
         "implementation"("org.springframework.boot:spring-boot-starter-webflux")
     }
 }
