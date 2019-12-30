b92d26d60ae50344ee57db24cb672f0b1f9739f1
Matt Glover <mglover@8thlight.com>
2019-12-30 16:41:41 +0000

    Simplify RouteConfigurationTest
    The first version of this test uses various Spring annontation which
    starts up a Spring environment in order to run the test. This can be
    useful in some circumstances, but is not needed here.
    
    This updated implementation only dependency is WebTestClient, which
    provides synthetic request and response objects. The test is more
    explicit and should run faster as it does not need a Spring context.

--- app/src/test/kotlin/com/matthewglover/app/RouteConfigurationTest.kt
+++ app/src/test/kotlin/com/matthewglover/app/RouteConfigurationTest.kt
@@ -2,22 +2,16 @@ package com.matthewglover.app
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
-import org.springframework.context.annotation.Import
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.reactive.server.WebTestClient
 
-
-@ExtendWith(SpringExtension::class)
-@WebFluxTest
-@Import(RouteConfiguration::class)
 internal class RouteConfigurationTest {
-    @Autowired private lateinit var client: WebTestClient
 
     @Test
     internal fun `GET greet returns 200 OK`() {
+        val client = WebTestClient
+            .bindToController(RouteConfiguration().routes())
+            .build()
+
         client.get()
             .uri("/greet")
             .exchange()
